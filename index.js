6dfa7e1d926069271e88e74d57399fd5e8497070
Melody <reticivis@gmail.com>
2024-05-16 20:07:55 -0500

    yippee

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import * as THREE from "./libs/three.module.min.js";
-import {GLTFLoader} from './libs/GLTFLoader.js';
+import * as THREE from "./libs/three/three.module.min.js";
+import {GLTFLoader} from './libs/three/GLTFLoader.min.js';
+import {RoomEnvironment} from "./libs/three/RoomEnvironment.min.js";
 
+let ready = false;
 
 const scene = new THREE.Scene();
 
@@ -14,36 +16,32 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 const fov = 50;
 
 const camera = new THREE.PerspectiveCamera(fov, document.documentElement.clientWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
+camera.position.z = 5;
+
+let logo;
+
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+scene.background = new THREE.Color( 0 );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+const logomaterial = new THREE.MeshStandardMaterial({});
+// scene.background = new THREE.Color(0xD3E8F0);
+
 const loader = new GLTFLoader();
-let fellow;
-let ready = false;
-
-const white = "data:image/bmp;base64,Qk06AAAAAAAAADYAAAAoAAAAAQAAAAEAAAABABgAAAAAAAQAAADEDgAAxA4AAAAAAAAAAAAA////AA=="
-let cube = new THREE.CubeTextureLoader().load([
-    white,white,white,white,white,white
-]);
-scene.background = new THREE.Color(0xD3E8F0);
-
-function applyenvmap() {
-    // this'll be called twice so both the fellow and the env map can load at once, on the 2nd call they'll both be
-    // loaded and the map will actually be applied
-    // if (!envmap || !fellow) return
-    fellow.traverse(function (child) {
+loader.load('./assets/reticivis.glb', function (gltf) {
+    logo = gltf.scene;
+    logo.traverse(function (child) {
         if (child instanceof THREE.Mesh) {
-            child.material.envMapIntensity = 2.0;
-            child.material.envMap = cube;
-            child.material.needsUpdate = true;
+            child.material = logomaterial;
         }
     });
-    scene.add(fellow)
-}
-
-loader.load('./assets/reticivis.glb', function (gltf) {
-    fellow = gltf.scene;
-    applyenvmap()
-    // scene.add(fellow);
-    // console.debug(fellow)
+    // logo.position.x = -1.65;
+    // logo.position.y = -.7;
+    // console.debug(logo)
+    scene.add(logo)
+    // scene.add(logo);
+    console.debug(logo);
+    onWindowResize();
+    animate();
 }, console.debug, console.error);
 
 
@@ -53,7 +51,7 @@ function onWindowResize() {
     camera.aspect = document.documentElement.clientWidth / window.innerHeight;
 
     // stolen from https://discourse.threejs.org/t/keeping-an-object-scaled-based-on-the-bounds-of-the-canvas-really-battling-to-explain-this-one/17574/10
-    // resizes the fellow to scale to the viewport
+    // resizes the logo to scale to the viewport
     if (camera.aspect > 1) {
         // window too large
         camera.fov = fov;
@@ -69,6 +67,22 @@ function onWindowResize() {
     renderer.setSize(document.documentElement.clientWidth, window.innerHeight);
     camera.updateProjectionMatrix();
 }
+
+function arrayfromfunc(len, func) {
+    return Array.from({length: len}, func);
+}
+
+const curve = new THREE.CatmullRomCurve3(
+    arrayfromfunc(100, () =>
+        new THREE.Vector3(randomFloat(-0.4, 0.4),randomFloat(-0.4, 0.4),randomFloat(-0.05, 0.05))
+    ),
+    true,
+    "centripetal",
+    0.4
+);
+function randomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
 const start = Date.now();
 let last = start;
 function animate() {
@@ -77,6 +91,9 @@ function animate() {
     let delta = Date.now() - last;
     last = Date.now();
 
+    let randvec = curve.getPoint(elapsed / 1000 / 100);
+    logo.setRotationFromEuler(new THREE.Euler().setFromVector3(randvec));
+
 
     // render
     requestAnimationFrame(animate);
@@ -84,4 +101,3 @@ function animate() {
 }
 
 document.body.prepend(renderer.domElement);
-animate();
